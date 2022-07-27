28eb3751ec2f2436122dd85f9c868f3818638996
Adeola Aziba <k2delly@gmail.com>
2022-07-27 14:02:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector(".toggle-btn")
+const sidebar = document.querySelector(".sidebar")
+const closeBtn = document.querySelector(".close-btn")
+
+toggleBtn.addEventListener("click", function(){
+sidebar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener("click", function(){
+sidebar.classList.remove('show-sidebar')
+})
